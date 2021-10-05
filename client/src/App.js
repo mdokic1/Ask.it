07a9ca498fdfabc323e64bf44380e52cce040446
mdokic1 <mdokic1@etf.unsa.ba>
2021-10-05 03:14:55 +0200

    Bugs fixed

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   async function isAuth(){
     try {
-      const response = await fetch("http://localhost:5000/auth/is-verified", {
+      const response = await fetch("/auth/is-verified", {
         method: "GET",
         headers: {token : localStorage.token}
       });
